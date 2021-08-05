5b687f2ad11e3311ecc6eb15e38a44f022e83a04
Pranay <pranay.chahare@hbwsl.com>
2021-08-05 17:19:18 +0530

    Share pae design added

--- src/Components/Share/index.js
+++ src/Components/Share/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { shortcodeTypes } from "../../Data";
 import Tabs from "../../HelperComponents/Tabs";
+import '../../scss/share.scss';
+
 
 export default function Share() {
     
@@ -19,26 +21,47 @@ export default function Share() {
 		}
 	}, [checked])
 	const handleRadioChange = (e) => {
+		var clickedLabel = document.querySelectorAll('.contentTypeLabel');
+		clickedLabel.forEach(function(label){
+			label.parentNode.classList.remove('contentType-element-active');
+		});
+		e.currentTarget.parentNode.classList.add('contentType-element-active');
 		setChecked(e.target.value);
 	}
 	
 	return (
         <div className="Share">
-            <div className="shareTabs">Share ShortCode</div>
-            <div className="shareShortcodeSettings">
-				<div className="contentShortcodeLabel">
-					<label htmlFor="contentShortcode">Content ShortCode: </label>
-					<button>Copy Shortcode</button>
+            <div className="shareTabs">
+				<div className="shareTabs-element">
+					<h3>Share ShortCode</h3>
+					<img src={require('../Build/BuildImages/arrowRight.png')}></img>
 				</div>
-				<input type="text" id="contentShortcode" disabled value={shortcode} />
+			</div>
+            <div className="shareShortcodeSettings">
+				<div className="contentShortcodeLabel-container">
+					<div className="contentShortcodeLabel">
+						<h3>Content ShortCode: </h3>
+						<button>Copy Shortcode</button>
+					</div>
+					<input type="text" id="contentShortcode" disabled value={shortcode} />
+				</div>
+
 				<div className="contentTypes">
-					Select Content Shortcode Type:
-					{types.map(function(item, i) {
-						return <div key={item.id}>
-							<label htmlFor={item.id} className={item.id === checked ? 'contentTypeLabel active' : 'contentTypeLabel'}>{item.name}</label>
-							<input type="radio" name="contentType" className="contentTypeRadio" checked={item.id === checked} onChange={handleRadioChange} value={item.id} id={item.id} />
-						</div>
-					})}
+					<div className="contentTypesLabel">
+						<h3>
+							Select Content Shortcode Type:
+						</h3>
+					</div>
+					<div className="contentTypes-container">
+						{types.map(function(item, i) {
+							
+							return <div key={item.id} className={item.id === checked ? 'contentType-element contentType-element-active' : 'contentType-element'}>
+								<label htmlFor={item.id} className={item.id === checked ? 'contentTypeLabel active' : 'contentTypeLabel'}>{item.name}</label>
+								<input type="radio" name="contentType" className="contentTypeRadio" checked={item.id === checked} onChange={handleRadioChange} value={item.id} id={item.id} disabled={item.id === 'reponsive' ? '' : 'disabled'} />
+							</div>
+						})}
+					</div>
+
 				</div>
 				<div className="contentHelpText">
 					{helpText}
