95c99e778b6c8124209c0b36306e2a3a5bda053a
premsurana <premx81@gmail.com>
2021-08-31 15:50:11 +0530

    Removed Conflicting test cases

--- jest-setup.js
+++ jest-setup.js
@@ -4,6 +4,15 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { enableFetchMocks } from 'jest-fetch-mock'
 import "@testing-library/jest-dom/extend-expect";
 
+
+global.wp = {
+	hooks: {
+		applyFilters: function(num1, num2, callback) {},
+		doAction: function(num1, num2, callback) {}
+	}
+}
+
+
 enableFetchMocks();
 
 configure({adapter: new Adapter()});
