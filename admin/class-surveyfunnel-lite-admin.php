cf14e05366360239cf8a0e0d5f650f5e609e3392
Pranay <pranay.chahare@hbwsl.com>
2021-09-03 11:15:27 +0530

    front end fix

--- admin/class-surveyfunnel-lite-admin.php
+++ admin/class-surveyfunnel-lite-admin.php
@@ -910,25 +910,37 @@ class Surveyfunnel_Lite_Admin {
 	 * @param array $attributes survey attributes.
 	 */
 	public function surveyfunnel_lite_gutenberg_display_single_survey( $attributes ) {
-		$survey_atts       = array();
+		$survey_atts = array();
+		$shortcode   = '[surveyfunnel_lite_survey';
+
 		$survey_atts['id'] = 0;
 		if ( isset( $attributes['survey_id'] ) ) {
 			$survey_atts['id'] = $attributes['survey_id'];
 		}
+		$shortcode          .= ' id="' . $survey_atts['id'] . '"';
 		$survey_atts['type'] = 'responsive';
 		if ( isset( $attributes['survey_embed_type'] ) ) {
 			$survey_atts['type'] = $attributes['survey_embed_type'];
 		}
-		$survey_atts['width'] = '400px';
-		if ( isset( $attributes['survey_custom_width'] ) ) {
-			$survey_atts['width'] = $attributes['survey_custom_width'];
-		}
-		$survey_atts['height'] = '400px';
-		if ( isset( $attributes['survey_custom_height'] ) ) {
-			$survey_atts['height'] = $attributes['survey_custom_height'];
+		$shortcode .= ' type="' . $survey_atts['type'] . '"';
+
+		if ( 'custom' === $survey_atts['type'] ) {
+
+			$survey_atts['width'] = '400px';
+			if ( isset( $attributes['survey_custom_width'] ) ) {
+				$survey_atts['width'] = $attributes['survey_custom_width'];
+			}
+			$shortcode            .= ' width="' . $survey_atts['width'] . '"';
+			$survey_atts['height'] = '400px';
+			if ( isset( $attributes['survey_custom_height'] ) ) {
+				$survey_atts['height'] = $attributes['survey_custom_height'];
+			}
+			$shortcode .= ' height="' . $survey_atts['height'] . '"';
+
 		}
 
-		return Surveyfunnel_Lite_Public::surveyfunnel_lite_display_survey( $survey_atts );
+		$shortcode .= ']';
+		return $shortcode;
 	}
 
 	/**
