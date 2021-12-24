c167c848e0e2660255a7e1676e5941da84cde1ae
Rishabh <rishabhtiwari821@gmail.com>
2021-12-24 12:42:01 +0530

    Worked on ModalContentRight.test.js

--- src/JSTests/Components/Build/Elements/contentElements.test.js
+++ src/JSTests/Components/Build/Elements/contentElements.test.js
@@ -2,9 +2,10 @@
  * @jest-environment jsdom
  */
 import React from 'react';
-import { shallow } from 'enzyme';
-import { Answer } from "../../../../Components/Build/Elements/ContentElements";
-
+import { shallow,mount} from 'enzyme';
+import { Answer,Choices } from "../../../../Components/Build/Elements/ContentElements";
+import HOCChoices from "../../../../Components/Build/Elements/ContentElements";
+import {render} from '@testing-library/react'
 function ModalContentRight() {
 	return <></>
 }
@@ -14,12 +15,65 @@ function CloseModal() {
 }
 
 function saveToList() {
-
 }
+// const initialProps={
+// 	designCon:{
+// 		answerBorderColor: {
+// 			r: 0,
+// 			g: 0,
+// 			b: 0,
+// 			a: 1,
+// 		}
+// 	},
+// 	 currentElement:{
+// 		componentName: 'ShortAnswer',
+// 		currentlySaved: true,
+// 		title: '',
+// 		button: '',
+// 		description: '',
+// 		mandatory: false
+// 	}, 
+// 	ModalContentRight:{ModalContentRight},
+// 	saveToList:{saveToList}, 
+// 	CloseModal:{CloseModal},
+// 	type:"CONTENT_ELEMENTS"
+// }
+// const shallowWrapper = shallow(<Choices {...initialProps} />);
+// const mountWrapper = mount(<Choices {...initialProps} />);
+// const mountHOComponent = mount(<Choices {...initialProps} />);
 
+// const { rerender } = render(<Choices {...initialProps} />);
+// rerender(<Choices {...initialProps} />);
+// describe('Choices component should get rendered',()=>{
+// 	afterAll(() => {
+// 		shallowWrapper.unmount();
+// 		mountWrapper.unmount();
+// 	  });
+// 	it('shallowWrapper should render the short answer component', () => {
+		
+// 		const form = component.find('input').first();
+// 		const button = component.find('button').first();
+// 		button.props().onClick({ target: {
+// 			name: 'save'
+// 		}});
+
+// 		expect(component.state('error')).toBe('Please add title before saving.');
+
+// 		form.props().onChange({ target: {
+// 			name: 'title',
+// 			value: 'myValue'
+// 		}});
+
+// 		button.props().onClick({ target: {
+// 			name: 'save'
+// 		}});
+
+// 		expect(component.state('title')).toBe('myValue');
+// 		expect( component.exists( '.shortAnswer-input' ) ).toEqual(true);
+// 	} )
+// })
 describe( 'Answer component should get rendered', () => {
 	it('should render the short answer component', () => {
-
 		const component = shallow(<Answer designCon={{
 			answerBorderColor: {
 				r: 0,
@@ -34,7 +88,10 @@ describe( 'Answer component should get rendered', () => {
 			button: '',
 			description: '',
 			mandatory: false
-		}} ModalContentRight={ModalContentRight} saveToList={saveToList} CloseModal={CloseModal}  />)
+		}} 
+		ModalContentRight={ModalContentRight} 
+		saveToList={saveToList} 
+		CloseModal={CloseModal}  />)
 
 		const form = component.find('input').first();
 		const button = component.find('button');
