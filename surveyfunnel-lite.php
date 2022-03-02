3dce3aaa00409ad810267f4df5b3e2f0e95440e5
sanket <sanket@3n19m4.local>
2022-03-02 15:48:21 +0530

    updated readme and version number

--- surveyfunnel-lite.php
+++ surveyfunnel-lite.php
@@ -15,7 +15,7 @@
  * Plugin Name:       SurveyFunnel Lite
  * Plugin URI:        https://club.wpeka.com/
  * Description:       Boost your leads using quiz / survey lead techniques . Drag & drop form builder to launch quizzes within minutes.
- * Version:           1.1.1
+ * Version:           1.1.2
  * Author:            WPEka Club
  * Author URI:        https://club.wpeka.com
  * License:           GPL-2.0+
@@ -34,7 +34,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'SURVEYFUNNEL_LITE_VERSION', '1.1.1' );
+define( 'SURVEYFUNNEL_LITE_VERSION', '1.1.2' );
 
 if ( ! defined( 'SURVEYFUNNEL_LITE_PLUGIN_URL' ) ) {
 	define( 'SURVEYFUNNEL_LITE_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
