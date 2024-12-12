4dcddf817177af7553b32bd8488ada2b00039e06
Divya Narhe <divya.narhe@hbwsl.com>
2024-12-12 15:34:30 +0530

    updated changelog, readme and test compatibility with WP6.7

--- includes/class-surveyfunnel-lite.php
+++ includes/class-surveyfunnel-lite.php
@@ -70,7 +70,7 @@ class Surveyfunnel_Lite {
 		if ( defined( 'SURVEYFUNNEL_LITE_VERSION' ) ) {
 			$this->version = SURVEYFUNNEL_LITE_VERSION;
 		} else {
-			$this->version = '1.1.4';
+			$this->version = '1.1.5';
 		}
 		$this->plugin_name = 'surveyfunnel-lite';
 
